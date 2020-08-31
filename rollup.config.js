519b3603a0e73ba29f7c5d8eb0c8c488b5a7f666
Sébastien Côté <sebas642@gmail.com>
2020-08-31 11:28:25 -0500

    feat: Remove service worker

--- rollup.config.js
+++ rollup.config.js
@@ -84,23 +84,5 @@ export default {
 
 		preserveEntrySignatures: 'strict',
 		onwarn,
-	},
-
-	serviceworker: {
-		input: config.serviceworker.input(),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			commonjs(),
-			!dev && terser()
-		],
-
-		preserveEntrySignatures: false,
-		onwarn,
 	}
-
 };
